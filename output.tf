75fa87eb88d3f0a19688df8311205b87a65fa8b5
pinar15 <pdasdemir49@gmail.com>
2023-02-02 16:04:59 -0600

    change

---
+++ output.tf
@@ -0,0 +1,27 @@
+output "info" {
+  value = aws_vpc.example.default_security_group_id
+}
+
+output "id" {
+  value = aws_vpc.example.id
+}
+
+output "azs" {
+  value = data.aws_availability_zones.available.names
+}
+
+
+output list_of_users {
+    value = <<EOF
+
+
+        Today you have created the following users
+
+
+                "${aws_iam_user.lb["bob1"].arn}"
+
+
+
+
+    EOF
+}
